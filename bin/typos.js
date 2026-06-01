943850d800434943fb6771de6a4695b0300278c0
coderiaser <coderaiser@cloudcmd.io>
2026-06-01 23:24:38 +0300

    feature: typos.ai: simplify

--- bin/typos.js
+++ bin/typos.js
@@ -1,25 +1,14 @@
 #!/usr/bin/env node
 
-import {join} from 'node:path';
 import process, {cwd} from 'node:process';
-import {readFile} from 'node:fs/promises';
 import {readStdin} from 'redstd';
-import {resolveEvent} from '../lib/typos.js';
-import {parseTypos} from '../lib/parse.js';
-
-const CWD = cwd();
+import {typos} from '../lib/typos.js';
 
 const stdin = await readStdin();
 
 if (!stdin)
     process.exit();
 
-for (const event of parseTypos(stdin)) {
-    if (event.type !== 'typo')
-        continue;
-    
-    const filePath = join(CWD, event.path);
-    const file = await readFile(filePath, 'utf-8');
-    
-    await resolveEvent(event, file);
-}
+await typos({
+    cwd,
+});
