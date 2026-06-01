84a69c2b66f273059fdaf8e5dc8a83f9f9c71d70
coderiaser <coderaiser@cloudcmd.io>
2026-06-01 23:37:13 +0300

    fix: typos.ai: readStdin

--- bin/typos.js
+++ bin/typos.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
-import process, {cwd} from 'node:process';
+import {cwd, exit} from 'node:process';
 import {readStdin} from 'redstd';
 import {typos} from '../lib/typos.js';
 
-const stdin = await readStdin();
-
-if (!stdin)
-    process.exit();
-
 await typos({
     cwd,
+    readStdin,
+    exit,
 });
