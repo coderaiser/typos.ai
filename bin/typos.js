9f54140be39905b5b5988a181c71cfc9a1eb0f9a
coderiaser <coderaiser@cloudcmd.io>
2026-06-01 23:22:10 +0300

    fix: typos.ai: bin: cwd

--- bin/typos.js
+++ bin/typos.js
@@ -7,6 +7,8 @@ import {readStdin} from 'redstd';
 import {resolveEvent} from '../lib/typos.js';
 import {parseTypos} from '../lib/parse.js';
 
+const CWD = cwd();
+
 const stdin = await readStdin();
 
 if (!stdin)
@@ -16,7 +18,7 @@ for (const event of parseTypos(stdin)) {
     if (event.type !== 'typo')
         continue;
     
-    const filePath = join(cwd, event.path);
+    const filePath = join(CWD, event.path);
     const file = await readFile(filePath, 'utf-8');
     
     await resolveEvent(event, file);
