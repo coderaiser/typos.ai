06587f2c74563adc2a64ad9b945c80c1c42cfc6d
coderiaser <coderaiser@cloudcmd.io>
2026-06-01 23:16:14 +0300

    fix: typos.ai: parse

--- bin/typos.js
+++ bin/typos.js
@@ -5,15 +5,20 @@ import {readFile} from 'node:fs/promises';
 
 import {readStdin} from 'redstd';
 import {resolveEvent} from '../lib/typos.js';
+import {parseTypos} from '../lib/parse.js';
 
-const line = await readStdin();
+const stdin = await readStdin();
 
-if (!line)
+if (!stdin)
     process.exit();
 
-const event = JSON.parse(line);
+for (const event of parseTypos(stdin)) {
+    if (event.type !== 'typo')
+        continue;
+    
+    const filePath = join(cwd, event.path);
+    const file = await readFile(filePath, 'utf-8');
+    
+    await resolveEvent(event, file);
+}
 
-const filePath = join(cwd, event.path);
-const file = await readFile(filePath, 'utf-8');
-
-await resolveEvent(event, file);
